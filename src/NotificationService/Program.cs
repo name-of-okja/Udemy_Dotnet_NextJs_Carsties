ad04d0fbbc923ea214cf727553003d950bdf2d14
OkJa <nameOfOkJa@gmail.com>
2023-11-30 14:22:36 +0900

    End of section 12

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,32 @@
+using MassTransit;
+using NotificationService.Consumers;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// RabbitMQ
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+// SignalR
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
